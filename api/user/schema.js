1e3c187405370d59800480f4f095ae68a22b8dd0
Mr-Semicolon <yahiya.mahrous@gmail.com>
2021-05-03 07:15:43 +0200

    changes

--- api/user/schema.js
+++ api/user/schema.js
@@ -17,6 +17,7 @@ exports.createUserSchema = {
   socialMedia: Joi.string().max(255).required(),
   image: Joi.string().max(3000).required(),
   facebookOrTwitter: Joi.string().valid("Facebook", "Twitter").required(),
+  accountType: Joi.string().valid("User", "Coach").required(),
 };
 exports.loginUserSchema = {
   email: Joi.string().email().max(255).required(),
